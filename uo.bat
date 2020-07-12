13081e6952f067aced692992bd287a8bf65eae43
ivan deng <guohuadeng@hotmail.com>
2020-07-12 23:40:56 +0800

    update partner user

--- uo.bat
+++ uo.bat
@@ -1,3 +1,7 @@
 rem 更新odoo为本翻译
 xcopy .\source\odoo  d:\odoo13-x64\source\odoo /E /Y
 xcopy .\source\addons  d:\odoo13-x64\source\odoo\addons /E /Y
+
+rem 企业版
+
+xcopy .\source_ent  d:\pjweb\o13.erpapp.cn\addons_ent /E /Y
